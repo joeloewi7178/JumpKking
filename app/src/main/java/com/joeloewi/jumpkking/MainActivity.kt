c6537bfbc20d6889c56f8af6bc91e0ba03f6a96c
joeloewi7178 <joeloewi7178@gmail.com>
2022-06-08 23:34:28 +0900

    Initial Commit

---
+++ app/src/main/java/com/joeloewi/jumpkking/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.joeloewi.jumpkking
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.joeloewi.jumpkking.ui.theme.JumpKkingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JumpKkingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JumpKkingTheme {
+        Greeting("Android")
+    }
+}
