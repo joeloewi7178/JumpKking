3223b7c057d8c863ebc94b429e49864e1e286429
joeloewi7178 <joeloewi7178@gmail.com>
2022-06-26 23:18:16 +0900

    del: firebase ui dependency mod: make PageKey class to data class (equals issue)

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -70,8 +70,6 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:30.0.1"))
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.firebase:firebase-auth-ktx")
-
-    implementation("com.firebaseui:firebase-ui-firestore:8.0.0")
 }
 
 protobuf {
