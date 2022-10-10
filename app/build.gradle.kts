45fe2c8330d748dab55f57118a6d3891c4fab895
joeloewi7178 <joeloewi7178@gmail.com>
2022-10-10 17:08:23 +0900

    versionCode 9 versionName 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.joeloewi.jumpkking"
         minSdk = 21
         targetSdk = 33
-        versionCode = 8
-        versionName = "1.0.8"
+        versionCode = 9
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
