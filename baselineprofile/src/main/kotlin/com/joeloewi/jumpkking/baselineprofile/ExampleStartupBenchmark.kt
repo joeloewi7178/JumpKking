bfc6718a2062b8522ce7c6ae518dfc9b4877adc9
joeloewi7178 <joeloewi7178@gmail.com>
2023-12-27 23:44:59 +0900

    versionCode 15 versionName 1.0.15
    add: baseline profile generator
    mod: emulator parameter

--- baselineprofile/src/main/kotlin/com/joeloewi/jumpkking/baselineprofile/ExampleStartupBenchmark.kt
+++ baselineprofile/src/main/kotlin/com/joeloewi/jumpkking/baselineprofile/ExampleStartupBenchmark.kt
@@ -1,5 +1,7 @@
 package com.joeloewi.jumpkking.baselineprofile
 
+import androidx.benchmark.macro.BaselineProfileMode
+import androidx.benchmark.macro.CompilationMode
 import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
@@ -23,16 +25,38 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class ExampleStartupBenchmark {
     @get:Rule
-    val benchmarkRule = MacrobenchmarkRule()
+    val rule = MacrobenchmarkRule()
 
     @Test
-    fun startup() = benchmarkRule.measureRepeated(
-        packageName = "com.joeloewi.jumpkking",
-        metrics = listOf(StartupTimingMetric()),
-        iterations = 5,
-        startupMode = StartupMode.COLD
-    ) {
-        pressHome()
-        startActivityAndWait()
+    fun startupCompilationNone() =
+        benchmark(CompilationMode.None())
+
+    @Test
+    fun startupCompilationBaselineProfiles() =
+        benchmark(CompilationMode.Partial(BaselineProfileMode.Require))
+
+    private fun benchmark(compilationMode: CompilationMode) {
+        rule.measureRepeated(
+            packageName = "com.joeloewi.jumpkking",
+            metrics = listOf(StartupTimingMetric()),
+            compilationMode = compilationMode,
+            startupMode = StartupMode.COLD,
+            iterations = 10,
+            setupBlock = {
+                pressHome()
+            },
+            measureBlock = {
+                startActivityAndWait()
+
+                // TODO Add interactions to wait for when your app is fully drawn.
+                // The app is fully drawn when Activity.reportFullyDrawn is called.
+                // For Jetpack Compose, you can use ReportDrawn, ReportDrawnWhen and ReportDrawnAfter
+                // from the AndroidX Activity library.
+
+                // Check the UiAutomator documentation for more information on how to
+                // interact with the app.
+                // https://d.android.com/training/testing/other-components/ui-automator
+            }
+        )
     }
 }
