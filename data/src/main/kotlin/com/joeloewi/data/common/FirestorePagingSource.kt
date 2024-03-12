893e74afbc9fe46e5eae3ecdf0b0c30db941eb72
joeloewi7178 <joeloewi7178@gmail.com>
2024-03-12 17:28:04 +0900

    mod: paging logic

--- data/src/main/kotlin/com/joeloewi/data/common/FirestorePagingSource.kt
+++ data/src/main/kotlin/com/joeloewi/data/common/FirestorePagingSource.kt
@@ -19,33 +19,26 @@ class FirestorePagingSource(
             val key = params.key
             val documentsSnapshots = (key?.getPageQuery(query, params.loadSize)?.get(source)
                 ?: query.limit(params.loadSize.toLong())[source]).await().documents
-            val nextKey = PageKey(documentsSnapshots.lastOrNull(), null)
-            val prevKey = PageKey(null, documentsSnapshots.firstOrNull())
+            val lastItem = documentsSnapshots.lastOrNull()
+            val firstItem = documentsSnapshots.firstOrNull()
+            val nextKey = lastItem?.let { PageKey(it, null) }
+            val prevKey = firstItem?.let { PageKey(null, it) }
             val original = query.get(source).await()
 
             LoadResult.Page(
                 data = documentsSnapshots,
-                prevKey = if (key == null) {
-                    null
-                } else {
-                    prevKey
-                },
-                nextKey = if (documentsSnapshots.isEmpty()) {
-                    null
-                } else {
-                    nextKey
-                },
-                itemsBefore = key?.endBefore?.let {
-                    original.indexOf(it)
-                } ?: 0,
-                itemsAfter = key?.startAfter?.let {
-                    original.reversed().indexOf(it)
-                } ?: 0
+                prevKey = prevKey,
+                nextKey = nextKey,
+                itemsBefore = original.indexOf(firstItem).takeIf { it != -1 } ?: 0,
+                itemsAfter = original.reversed().indexOf(lastItem).takeIf { it != -1 } ?: 0,
             )
         } catch (cause: Throwable) {
             LoadResult.Error(cause)
         }
     }
 
-    override fun getRefreshKey(state: PagingState<PageKey, DocumentSnapshot>): PageKey? = null
+    override fun getRefreshKey(state: PagingState<PageKey, DocumentSnapshot>): PageKey? =
+        with(state) {
+            anchorPosition?.let { closestPageToPosition(it)?.prevKey }
+        }
 }
